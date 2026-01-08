78e3332f59e6201b5a72046c00abe65b080b637a
Leandro SQ <11012599+LeandroSQ@users.noreply.github.com>
2026-01-08 03:12:49 -0300

    Fix build script to create dist directory and compile

--- tools/build.sh
+++ tools/build.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 set -e
-mkdir -r ../dist
-gcc ../src/main.c     \
-    ../src/util.c     \
-    ../src/vector.c   \
-    ../src/parser.c   \
-    -o ../dist/buzzer \
-    -llgpio           \ # Link LGPIO
-    -lm               \ # Link math
-    -Wall             \
-    -O2               \
-    -v
+
+mkdir -p dist
+
+# If -v is one of the args, add -DVERBOSE define
+VERBOSE_FLAG=""
+for arg in "$@"; do
+  if [ "$arg" = "-v" ]; then
+    VERBOSE_FLAG="-DVERBOSE"
+    break
+  fi
+done
+
+gcc src/main.c     \
+    src/util.c     \
+    src/vector.c   \
+    src/parser.c   \
+    -o dist/buzzer \
+    -llgpio        \
+    -lm            \
+    -Wall          \
+    -O2            \
+    $VERBOSE_FLAG  \
+    -v             \
+    "$@"
